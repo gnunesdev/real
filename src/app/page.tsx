368ada29cde8267fc735e48bdfef92adc03e2e66
gnunesdev <gnunes.programmer@gmail.com>
2024-08-25 20:02:45 -0300

    feat: bootstrap app

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <div className="w-full mt-8">
+          <h1 className="text-center text-6xl font-bold bg-gradient-to-b from-green-400 to-yellow-300 text-transparent bg-clip-text">
+            O Real
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+}
